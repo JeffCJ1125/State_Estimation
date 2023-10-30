7a8cc175ae18e754c3708641f4b1db8dedb701f5
jeffcjwindows <jeffcj1125@gmail.com>
2023-10-29 20:41:06 -0700

    apply pylint for coding style

--- Least_square_method/Least_square_example_resistor.py
+++ Least_square_method/Least_square_example_resistor.py
@@ -1,12 +1,16 @@
-import numpy as np
+"""
+This module provides example of least square method example
+"""
+
 import random as rd
+import numpy as np
 import matplotlib.pyplot as plt
 
 # set the idea R
-ideaR = 8
+IDEA_R = 8
 # set the idea measure based on idea R
 idea_i_measures = [rd.uniform(0.1, 0.8) for i in range(5)]
-idea_v_measures = [i * ideaR for i in idea_i_measures]
+idea_v_measures = [i * IDEA_R for i in idea_i_measures]
 
 # add measure noise
 v_measures = np.array([v + rd.uniform(-0.0, 0.0) for v in idea_v_measures])
@@ -20,7 +24,7 @@ x_hat = np.linalg.inv(H.T @ H) @ H.T @ y
 print(f"x_hat {x_hat}")
 
 I_line = np.arange(0, 0.8, 0.1).reshape(8, 1)
-V_line = ideaR * I_line
+V_line = IDEA_R * I_line
 
 plt.scatter(i_measures, v_measures)
 plt.plot(I_line, V_line)
--- Least_square_method/Weighted_Least_Square_example_resistor.py
+++ Least_square_method/Weighted_Least_Square_example_resistor.py
@@ -1,30 +1,34 @@
-import numpy as np
+"""
+This module provides example of Weighted Least Square method example
+"""
+
 import math
 import random as rd
+import numpy as np
 import matplotlib.pyplot as plt
 
 # set the idea R
-ideaR = 5
+IDEA_R = 5
 # set the idea measure based on idea R
 idea_i_measures = [rd.uniform(0.1, 0.8) for i in range(8)]
-idea_v_measures = [i * ideaR for i in idea_i_measures]
+idea_v_measures = [i * IDEA_R for i in idea_i_measures]
 
 # add bad multimeter noise based on variance
-bad_variance = 0.1
+BAD_VARIANCE = 0.1
 v_measures_bad = np.array(
-    [v + rd.gauss(0, math.sqrt(bad_variance)) for v in idea_v_measures[:4]]
+    [v + rd.gauss(0, math.sqrt(BAD_VARIANCE)) for v in idea_v_measures[:4]]
 )
 i_measures_bad = np.array(
-    [i + rd.gauss(0, math.sqrt(bad_variance)) for i in idea_i_measures[:4]]
+    [i + rd.gauss(0, math.sqrt(BAD_VARIANCE)) for i in idea_i_measures[:4]]
 )
 
 # add good multimeter noise based on variance
-good_variance = 0.001
+GOOD_VARIANCE = 0.001
 v_measures_good = np.array(
-    [v + rd.gauss(0, math.sqrt(good_variance)) for v in idea_v_measures[4:]]
+    [v + rd.gauss(0, math.sqrt(GOOD_VARIANCE)) for v in idea_v_measures[4:]]
 )
 i_measures_good = np.array(
-    [i + rd.gauss(0, math.sqrt(good_variance)) for i in idea_i_measures[4:]]
+    [i + rd.gauss(0, math.sqrt(GOOD_VARIANCE)) for i in idea_i_measures[4:]]
 )
 
 v_measures = np.concatenate((v_measures_bad, v_measures_good))
@@ -36,9 +40,8 @@ H = np.array([i_measures]).T
 
 # define R matrix
 R = np.zeros([8, 8])
-R[:4, :4] = np.eye(4) * bad_variance
-R[4:, 4:] = np.eye(4) * good_variance
-
+R[:4, :4] = np.eye(4) * BAD_VARIANCE
+R[4:, 4:] = np.eye(4) * GOOD_VARIANCE
 invR = np.linalg.inv(R)
 # print(f"R = \n {R}")
 
@@ -46,7 +49,7 @@ x_hat = np.linalg.inv(H.T @ invR @ H) @ H.T @ invR @ y
 print(f"x_hat {x_hat}")
 
 I_line = np.arange(0, 0.8, 0.1).reshape(8, 1)
-V_line = ideaR * I_line
+V_line = IDEA_R * I_line
 
 plt.scatter(i_measures_bad, v_measures_bad)
 plt.scatter(i_measures_good, v_measures_good, c="red")
